2dcc1e6319fa6dcb1dda50d6672b072d1a65219f
Daniel-Xu <xwd.forever@gmail.com>
2024-12-14 22:46:33 +0800

    Day06: rust version. I'm not feel good about the rust version

--- 2024/rust/day-06/src/part1.rs
+++ 2024/rust/day-06/src/part1.rs
@@ -1,6 +1,51 @@
+use std::collections::HashSet;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+    let grid: Vec<Vec<char>> = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let (m, n) = (grid.len(), grid[0].len());
+    let (mut startx, mut starty) = (0, 0);
+
+    for (i, row) in grid.iter().enumerate() {
+        for (j, &cell) in row.iter().enumerate() {
+            if cell == '^' {
+                startx = i;
+                starty = j;
+                break;
+            }
+        }
+    }
+
+    let mut visited = HashSet::new();
+    visited.insert((startx, starty));
+
+    let mut diri = 0;
+    let dir = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+    let (mut x, mut y) = (startx as i32, starty as i32);
+    let m = m as i32;
+    let n = n as i32;
+
+    while x >= 0 && x < m && y >= 0 && y < n {
+        let (dx, dy) = dir[diri];
+        let (nx, ny) = (x + dx, y + dy);
+        if nx < 0 || nx >= m || ny < 0 || ny >= n {
+            break;
+        }
+        if grid[nx as usize][ny as usize] == '#' {
+            diri += 1;
+            diri %= 4;
+        } else {
+            visited.insert((nx as usize, ny as usize));
+            x = nx;
+            y = ny;
+        }
+    }
+
+    Ok(format!("{}", visited.len()))
 }
 
 #[cfg(test)]
@@ -9,9 +54,17 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+        assert_eq!("41", process(input)?);
         Ok(())
     }
 }
--- 2024/rust/day-06/src/part2.rs
+++ 2024/rust/day-06/src/part2.rs
@@ -1,6 +1,117 @@
+use std::collections::HashSet;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+pub fn get_visited(input: &str) -> miette::Result<HashSet<(usize, usize)>> {
+    let grid: Vec<Vec<char>> = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let (m, n) = (grid.len(), grid[0].len());
+    let (mut startx, mut starty) = (0, 0);
+
+    for (i, row) in grid.iter().enumerate() {
+        for (j, &cell) in row.iter().enumerate() {
+            if cell == '^' {
+                startx = i;
+                starty = j;
+                break;
+            }
+        }
+    }
+
+    let mut visited = HashSet::new();
+    visited.insert((startx, starty));
+
+    let mut diri = 0;
+    let dir = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+    let (mut x, mut y) = (startx as i32, starty as i32);
+    let m = m as i32;
+    let n = n as i32;
+
+    while x >= 0 && x < m && y >= 0 && y < n {
+        let (dx, dy) = dir[diri];
+        let (nx, ny) = (x + dx, y + dy);
+        if nx < 0 || nx >= m || ny < 0 || ny >= n {
+            break;
+        }
+        if grid[nx as usize][ny as usize] == '#' {
+            diri += 1;
+            diri %= 4;
+        } else {
+            visited.insert((nx as usize, ny as usize));
+            x = nx;
+            y = ny;
+        }
+    }
+
+    Ok(visited)
+}
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut grid: Vec<Vec<char>> = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let (m, n) = (grid.len(), grid[0].len());
+    let (mut startx, mut starty) = (0, 0);
+
+    let mut past_path = get_visited(input)?;
+
+    for (i, row) in grid.iter().enumerate() {
+        for (j, &cell) in row.iter().enumerate() {
+            if cell == '^' {
+                startx = i;
+                starty = j;
+                break;
+            }
+        }
+    }
+    past_path.remove(&(startx, starty));
+    let mut res = 0;
+    // println!("past_path: {}", past_path.len());
+
+    let mut visited = HashSet::new();
+
+    for (px, py) in past_path {
+        grid[px][py] = '#';
+
+        let mut diri = 0;
+        let dir = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+        let (mut x, mut y) = (startx as i32, starty as i32);
+        let m = m as i32;
+        let n = n as i32;
+
+        // let mut visited = HashSet::new();
+        visited.clear();
+        visited.insert((startx, starty, diri));
+
+        while x >= 0 && x < m && y >= 0 && y < n {
+            let (dx, dy) = dir[diri];
+            let (nx, ny) = (x + dx, y + dy);
+            if nx < 0 || nx >= m || ny < 0 || ny >= n {
+                break;
+            }
+            if grid[nx as usize][ny as usize] == '#' {
+                diri += 1;
+                diri %= 4;
+            } else {
+                if visited.contains(&(nx as usize, ny as usize, diri)) {
+                    res += 1;
+                    break;
+                }
+                visited.insert((nx as usize, ny as usize, diri));
+                x = nx;
+                y = ny;
+            }
+        }
+
+        grid[px][py] = '.';
+    }
+
+    Ok(format!("{}", res))
 }
 
 #[cfg(test)]
@@ -9,9 +120,18 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+        assert_eq!("6", process(input)?);
+
         Ok(())
     }
 }
