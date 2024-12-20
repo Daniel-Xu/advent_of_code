072ba0c166323f8f6726beda91d6c23a4e44b6db
Daniel-Xu <xwd.forever@gmail.com>
2024-12-20 23:28:43 +0800

    Day09: rust version using btreemap

--- 2024/rust/day-09/src/part1.rs
+++ 2024/rust/day-09/src/part1.rs
@@ -1,6 +1,45 @@
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+    let input = input.trim();
+    let n = input
+        .chars()
+        .map(|c| c.to_digit(10).unwrap_or(0) as u64)
+        .sum::<u64>();
+
+    let mut v: Vec<i64> = vec![-1; n as usize];
+    let mut i = 0;
+    for (j, c) in input.chars().enumerate() {
+        let mut value = j as i64 / 2;
+        if j % 2 != 0 {
+            value = -1;
+        }
+
+        let cnt = c.to_digit(10).unwrap_or(0) as usize;
+        v[i..i + cnt].fill(value);
+        i += cnt;
+    }
+    let (mut i, mut j) = (0usize, (n - 1) as usize);
+    while i < j {
+        if v[i] != -1 {
+            i += 1;
+            continue;
+        }
+        if v[j] == -1 {
+            j -= 1;
+            continue;
+        }
+        v.swap(i, j);
+        i += 1;
+        j -= 1;
+    }
+    let res = v
+        .iter()
+        .enumerate()
+        .filter(|(_, &val)| val != -1)
+        .map(|(i, &val)| val as u64 * i as u64)
+        .sum::<u64>();
+
+    Ok(res.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +48,9 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "2333133121414131402";
+        // let input = "12345";
+        assert_eq!("1928", process(input)?);
         Ok(())
     }
 }
--- 2024/rust/day-09/src/part2.rs
+++ 2024/rust/day-09/src/part2.rs
@@ -1,6 +1,55 @@
+use std::collections::{BTreeMap, VecDeque};
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<String> {
+    let input = input.trim();
+
+    let mut empty_m = BTreeMap::new();
+    let mut file_dec = VecDeque::new();
+
+    let mut inci = 0;
+    for (i, c) in input.chars().enumerate() {
+        let file_cnt = c.to_digit(10).unwrap() as usize;
+        if i % 2 == 0 {
+            let file_value = i as i64 / 2;
+            file_dec.push_back((file_value, (inci, file_cnt)));
+        } else {
+            empty_m.insert(inci, file_cnt);
+        };
+        inci += file_cnt;
+    }
+
+    let file_n = file_dec.len();
+    for _ in 0..file_n {
+        let (file_value, (file_i, file_cnt)) = file_dec.pop_back().unwrap();
+        let empty_entry = empty_m
+            .iter()
+            .find(|(start, cnt)| **start < file_i && **cnt >= file_cnt)
+            .map(|(k, _)| *k);
+
+        if let Some(empty_i) = empty_entry {
+            let empty_cnt = empty_m[&empty_i];
+            let letf = empty_cnt - file_cnt;
+
+            empty_m.remove(&empty_i);
+            if letf > 0 {
+                empty_m.insert(empty_i + file_cnt, letf);
+            }
+
+            file_dec.push_front((file_value, (empty_i, file_cnt)));
+        } else {
+            file_dec.push_front((file_value, (file_i, file_cnt)));
+        }
+    }
+
+    let mut res = 0;
+    for (file_value, (file_i, file_cnt)) in file_dec {
+        for i in 0..file_cnt {
+            res += (file_i + i) * file_value as usize;
+        }
+    }
+
+    Ok(res.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +58,8 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "2333133121414131402";
+        assert_eq!("2858", process(input)?);
         Ok(())
     }
 }
